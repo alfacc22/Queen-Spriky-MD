b0193f427abcd51a6aaa857f12efaedf9d547518
alfacc22 <alfccount79@gmail.com>
2024-12-14 16:56:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'ujZhCL4C#sgYQSiNGn_Yw9jgaGHD2lnyMg4i9M831-xQwTv-RseI', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'ruxEETJI#SGFyT558HYSjt34vEWNGenncHKChUai5ptSmPfe1XK8', // Enter Your Session ID
     MONGODB: process.env.MONGODB || 'mongodb+srv://hasiyamno:20020421@cluster0.7xoks.mongodb.net/',// Enter Your MongoDB URL
     OWNER_NUMBER: process.env.OWNER_NUMBER || '94763118285',    // Enter Your Owner Number
 };
